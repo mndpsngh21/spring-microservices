5c88b609b46d9b035ca679080b26af2045c2f8fa
mndpsngh21 <mndpsngh21@gmail.com>
2021-05-11 08:41:38 +0530

    Initial Code structure

---
+++ CentralServer/src/main/java/com/mandeep/microservices/CentralServer/CentralServerApplication.java
@@ -0,0 +1,13 @@
+package com.mandeep.microservices.CentralServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentralServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentralServerApplication.class, args);
+	}
+
+}
---
+++ CentralServer/src/test/java/com/mandeep/microservices/CentralServer/CentralServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mandeep.microservices.CentralServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentralServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
