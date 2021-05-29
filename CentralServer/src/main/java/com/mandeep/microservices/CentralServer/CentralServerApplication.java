e654a8506cc7edcbcac284843959c61f54d018bc
mndpsngh21 <mndpsngh21@gmail.com>
2021-05-29 15:26:00 +0530

    Enable Eureka Server on Central Server application

--- CentralServer/src/main/java/com/mandeep/microservices/CentralServer/CentralServerApplication.java
+++ CentralServer/src/main/java/com/mandeep/microservices/CentralServer/CentralServerApplication.java
@@ -2,8 +2,27 @@ package com.mandeep.microservices.CentralServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+
+/**
+ * 
+ * @author Mandeep
+ * @EnableEurekaServer annotation marks this application as eureka server
+ *   We need to add below dependency:
+ *    <dependency>
+			<groupId>org.springframework.cloud</groupId>
+			<artifactId>spring-cloud-starter-netflix-eureka-server</artifactId>
+		</dependency> 
+		
+		By default, the registry also tries to register itself, so you need to disable that behavior as well.
+		We need to add below properties for same.
+		 - eureka.client.register-with-eureka=false
+         - eureka.client.fetch-registry=false
+ 
+ */
+@EnableEurekaServer
 
 @SpringBootApplication
 public class CentralServerApplication {
--- CentralServer/src/main/java/com/mandeep/microservices/CentralServer/Controller/StockController.java
+++ CentralServer/src/main/java/com/mandeep/microservices/CentralServer/Controller/StockController.java
@@ -3,9 +3,12 @@ package com.mandeep.microservices.CentralServer.Controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import com.mandeep.microservices.CentralServer.Models.GenericReponse;
+import com.mandeep.microservices.CentralServer.Models.stocks.Stock;
 import com.mandeep.microservices.CentralServer.Models.stocks.StockListResponse;
 
 
@@ -20,6 +23,23 @@ RestTemplate restTemplate;
 public StockListResponse getBrokerList()
 {
 	ResponseEntity<StockListResponse> stockListResponse= restTemplate.getForEntity("http://localhost:8100/stock/", StockListResponse.class);  	
+	return stockListResponse.getBody();
+}
+
+
+
+@GetMapping(path = "/SampleStockObject")	
+public Stock getSampleData()
+{
+	ResponseEntity<Stock> stockListResponse= restTemplate.getForEntity("http://localhost:8100/stock/SampleStockObject/", Stock.class);  	
+	return stockListResponse.getBody();
+}
+
+
+@PostMapping(path="/")
+public GenericReponse saveNewStock(Stock stock)
+{
+	ResponseEntity<GenericReponse> stockListResponse= restTemplate.postForEntity("http://localhost:8100/SampleStockObject/",stock, GenericReponse.class);  	
 	return stockListResponse.getBody();
 }
 	
